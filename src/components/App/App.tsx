e995297ade266d3fb37917541cbc30721bc141eb
Maks Shkafar <m.shkafar@gmail.com>
2025-07-17 22:08:35 +0200

    feat: add MovieGrid component to display movie list

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,25 @@
 import SearchBar from "../SearchBar/SearchBar";
 import styles from "./App.module.css";
 import { useState } from "react";
+import { fetchQuery } from "../../services/movieService";
+import type { Movie } from "../../types/movie";
+import MovieGrid from "../MovieGrid/MovieGrid";
 
 export default function App() {
-  const handleSearch = (searchMovie: string) => {
-    const [movie, setMovie] = useState;
+  const [movie, setMovie] = useState<Movie[]>([]);
+
+  const handleSearch = async (searchMovie: string) => {
+    const data = await fetchQuery(searchMovie);
+    setMovie(data);
+  };
+
+  const handleClick = () => {
+    console.log(movie);
   };
   return (
     <div className={styles.app}>
       <SearchBar onSubmit={handleSearch} />
+      <MovieGrid movies={movie} onSelect={handleClick} />
     </div>
   );
 }
--- src/components/MovieGrid/MovieGrid.tsx
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -1,6 +1,5 @@
-// Логика отображения списка фильмов по запросу
-// В Пропс этого компонента передаем стейт и типизумем по интерфесу Муви
 import type { Movie } from "../../types/movie";
+import styles from "./MovieGrid.module.css";
 
 interface MovieGridProps {
   onSelect: () => void;
@@ -8,18 +7,19 @@ interface MovieGridProps {
 }
 
 export default function MovieGrid({ onSelect, movies }: MovieGridProps) {
+  const imagesBaseUrl = "https://image.tmdb.org/t/p/w500/";
   return (
-    <ul>
+    <ul className={styles.grid}>
       {movies.map((movie) => (
-        <li>
-          <div className={css.card} onClick={onSelect}>
+        <li key={movie.id} onClick={onSelect}>
+          <div className={styles.card} onClick={onSelect}>
             <img
-              className={css.image}
-              src="https://image.tmdb.org/t/p/w500/poster-path"
-              alt="movie title"
+              className={styles.image}
+              src={imagesBaseUrl + movie.poster_path}
+              alt={movie.title}
               loading="lazy"
             />
-            <h2 className={css.title}>Movie title</h2>
+            <h2 className={styles.title}>{movie.title}</h2>
           </div>
         </li>
       ))}
